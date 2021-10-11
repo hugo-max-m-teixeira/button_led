d45180af5f7f3596aee9ae263ba9089de64605e5
hugo-max-m-teixeira <hugomaxmontelesteixeira@gmail.com>
2021-10-11 16:50:08 -0300

    button implemented

--- led_button/led_button.ino
+++ led_button/led_button.ino
@@ -1,14 +1,32 @@
 #define pin_led 13
+#define pin_button 8
+
+bool last_state = true;
+bool state_led = false;
 
 void setup (){
 	pinMode(pin_led, OUTPUT);
-
+	pinMode(pin_button, INPUT_PULLUP);
 }
 
 void loop (){
-	digitalWrite(pin_led, HIGH);
-	delay(200);
-	digitalWrite(pin_led, LOW);
-	delay(200);
+
+// botão apertado  = LOW
+// botão solto = HIGH
+
+// HIGH = true = 1
+// LOW = false = 0
+
+  if(!digitalRead(pin_button)){ // Botão pressionado
+    last_state = false;
+    delay(100);
+  }
+
+  if (digitalRead(pin_button) && !last_state){
+     state_led = !state_led;
+     last_state = true;
+  }
+
+	digitalWrite(pin_led, state_led);
 
 }
