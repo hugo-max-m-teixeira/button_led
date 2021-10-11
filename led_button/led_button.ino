1252687f120a4ec024ff6a7c1f90fb432f8a3a17
hugo-max-m-teixeira <hugomaxmontelesteixeira@gmail.com>
2021-10-11 16:18:06 -0300

    blinking led

--- led_button/led_button.ino
+++ led_button/led_button.ino
@@ -1,12 +1,14 @@
-
+#define pin_led 13
 
 void setup (){
-
+	pinMode(pin_led, OUTPUT);
 
 }
 
 void loop (){
-
-
+	digitalWrite(pin_led, HIGH);
+	delay(200);
+	digitalWrite(pin_led, LOW);
+	delay(200);
 
 }
